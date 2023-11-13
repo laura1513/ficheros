744b431889201f1cd3bfe56c9ad23a8f8d09c9f1
laura1513 <laurajorques2003@gmail.com>
2023-11-13 12:45:38 +0100

    ejrecicio 5 del pdf, ObjectMapper

--- src/main/java/org/example/ejercicio5/Producto.java
+++ src/main/java/org/example/ejercicio5/Producto.java
@@ -1,7 +1,9 @@
 package org.example.ejercicio5;
 
-import lombok.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 @Getter
 @Setter
 @ToString
