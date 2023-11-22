f1f0e32ea6d2457666c2d5c9142d0325c169223c
laura1513 <laurajorques2003@gmail.com>
2023-11-22 08:41:26 +0100

    hacerlo con metodos

--- src/main/java/org/example/ejercicio1csv/MainFunko.java
+++ src/main/java/org/example/ejercicio1csv/MainFunko.java
@@ -4,13 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class Main {
+public class MainFunko {
     public final static String COMMA_DELIMITER = ",";
     public static void main(String[] args) throws IOException {
         String[] funk;
@@ -30,51 +28,57 @@ public class Main {
             }
 
             System.out.print("El funko más caro es ");
-            String nom = lista.stream().max(Comparator.comparingDouble(Funko::getPrecio)).get().getNombre();
-            double pre = lista.stream().max(Comparator.comparingDouble(Funko::getPrecio)).get().getPrecio();
-            System.out.println(nom + " y cuesta " + pre + "€");
-
-            System.out.println();
+            funkoMasCaro(lista);
 
             System.out.print("Media de precio: ");
-            double media = lista.stream().mapToDouble(Funko::getPrecio).average().getAsDouble();
-            System.out.println(media + "€");
-
-            System.out.println();
+            System.out.println(mediaDePrecios(lista) + "€");
 
             System.out.println("Funkos por modelo:");
-            Map<String, List<Funko>> porModelo = lista.stream().collect(Collectors.groupingBy(Funko::getModelo));
-            for (Map.Entry<String, List<Funko>> p : porModelo.entrySet()) {
+            for (Map.Entry<String, List<Funko>> p : listaPorModelo(lista).entrySet()) {
                 System.out.println(p);
             }
 
-            System.out.println();
-
             System.out.println("Funkos por modelo:");
-            Map<String, Long> numPorModelo = lista.stream().collect(Collectors.groupingBy(Funko::getModelo, Collectors.counting()));
-            System.out.println(numPorModelo);
-
-            System.out.println();
+            System.out.println(numPorModelo(lista));
 
             System.out.println("Funkos scados en 2023:");
-            List<Funko> funko2023 = lista.stream().filter(f -> f.getFecha_lanzamiento().getYear() == 2023).toList();
-            for (Funko f: funko2023) {
+            for (Funko f: funkosSacadosEn2023(lista)) {
                 System.out.println(f);
             }
-
-            System.out.println();
 
             System.out.println("BACKUP:");
-            Funko.backup(lista, "backup.dat");
-            List<Funko> backupFunko = Funko.restore("backup.dat");
-            for (Funko f : backupFunko) {
-                System.out.println(f);
-            }
+            backupFunko(lista);
 
         } catch (FileNotFoundException e) {
             System.out.println("Fichero no encontrado");
         } catch (IOException e) {
             System.out.println("Error");
+        }
+    }
+
+    public static void funkoMasCaro(List<Funko> lista) {
+        String nom = lista.stream().max(Comparator.comparingDouble(Funko::getPrecio)).get().getNombre();
+        double pre = lista.stream().max(Comparator.comparingDouble(Funko::getPrecio)).get().getPrecio();
+        System.out.println(nom + " y cuesta " + pre + "€");
+    }
+    public static double mediaDePrecios(List<Funko> lista) {
+        return lista.stream().mapToDouble(Funko::getPrecio).average().getAsDouble();
+    }
+    public static Map<String, List<Funko>> listaPorModelo(List<Funko> lista) {
+        return (lista.stream().collect(Collectors.groupingBy(Funko::getModelo)));
+    }
+
+    public static Map<String, Long> numPorModelo(List<Funko> lista) {
+        return (lista.stream().collect(Collectors.groupingBy(Funko::getModelo, Collectors.counting())));
+    }
+    public static List<Funko> funkosSacadosEn2023(List<Funko> lista) {
+        return (lista.stream().filter(f -> f.getFecha_lanzamiento().getYear() == 2023).toList());
+    }
+    public static void backupFunko(List<Funko> lista) {
+        Funko.backup(lista, "backup.dat");
+        List<Funko> backupFunko = Funko.restore("backup.dat");
+        for (Funko f : backupFunko) {
+            System.out.println(f);
         }
     }
 }
