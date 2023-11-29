f66ba7cf9af5761e231ef3061044bb4efae56a81
laura1513 <laurajorques2003@gmail.com>
2023-11-29 13:09:14 +0100

    Se empieza la conexión con apis

---
+++ src/main/java/org/example/openAPI/MainAPI.java
@@ -0,0 +1,23 @@
+package org.example.openAPI;
+
+import com.fasterxml.jackson.core.json.JsonReadContext;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainAPI {
+    public static void main(String[] args) {
+        //https://pokeapi.co/api/v2/
+        try {
+            URL url=new URL("https://pokeapi.co/api/v2/pokemon/");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void listarJSON(URL url) throws IOException {
+
+    }
+}
